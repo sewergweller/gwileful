07503e9425ff607bfc103e9ec1327bd19b73c9be
Gwynsav <n0n3ofyourbus1nes5@outlook.com>
2026-01-12 14:34:25 +0100

    tmp

--- helpers.lua
+++ helpers.lua
@@ -1,3 +1,7 @@
+-- local lgi   = require('lgi')
+-- local cairo = lgi.cairo
+-- local gdk   = lgi.Gdk
+
 local _H = {}
 
 -- I feel like YanDev saying "I wish there was a better way to do this"...
@@ -60,5 +64,74 @@ function _H.dir_exists(path)
    end
    return (_H.exists(path) and not _H.file_exists(path))
 end
+
+-- function _H.downscale(path, w, h)
+--    if not _H.file_exists(path) then return end
+--    local target_r = w / h
+--
+--    -- Create a surface for the image.
+--    local image = gdk.Pixbuf.new_from_file(path)
+--    local source = {}
+--    source.surf = cairo.ImageSurface.create(cairo.Format.ARGB32, image.width, image.height)
+--    source.cr   = cairo.Context(source.surf)
+--    gdk.cairo_set_source_pixbuf(source.cr, image, 0, 0)
+--    source.cr:paint()
+--
+--    local src_w = source.surf:get_width()
+--    local src_h = source.surf:get_height()
+--    local src_r = src_w / src_h
+--
+--    local crop = {}
+--    --- If there is a ratio mismatch, we need to crop.
+--    if src_r ~= target_r then
+--       local new_w, new_h
+--       if src_r >= 1 then
+--          -- Wide image.
+--          new_h = src_h
+--          new_w = src_h * target_r
+--          crop.x = (src_w - new_w) / 2
+--          crop.y = 0
+--       else
+--          -- Tall image.
+--          new_h = math.floor(src_w / target_r)
+--          new_w = src_w
+--          crop.x = 0
+--          crop.y = (src_h - new_h) / 2
+--       end
+--       crop.surf = cairo.ImageSurface.create(cairo.Format.ARGB32, new_w, new_h)
+--    else
+--       crop.x = 0
+--       crop.y = 0
+--       crop.surf = cairo.ImageSurface.create(cairo.Format.ARGB32, image.width, image.height)
+--    end
+--    crop.cr = cairo.Context(crop.surf)
+--    crop.cr:set_source_surface(source.surf, -crop.x, -crop.y)
+--    crop.cr:paint()
+--
+--    --- If the image is already <= to the target, just return the surface.
+--    if src_w <= w and src_h <= h then
+--       source.cr:destroy()
+--       source.surf:destroy()
+--       return crop.surf
+--    end
+--
+--    -- Downscale.
+--    local scaled = {}
+--    scaled.surf = cairo.ImageSurface.create(cairo.Format.ARGB32, w, h)
+--    scaled.cr   = cairo.Context(scaled.surf)
+--
+--    scaled.cr:scale(w / crop.surf:get_width(), h / crop.surf:get_height())
+--    scaled.cr:set_source_surface(crop.surf, 0, 0);
+--    scaled.cr:paint()
+--
+--    -- Free the original image.
+--    source.cr:destroy()
+--    source.surf:destroy()
+--    -- And the crop.
+--    crop.cr:destroy()
+--    crop.surf:destroy()
+--
+--    return scaled
+-- end
 
 return _H
